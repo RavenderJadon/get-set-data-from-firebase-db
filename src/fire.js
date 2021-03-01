8b5d3ca53679d9b1e13630acd1a1db18420e5cef
Ravender Jadon <ravender@jnexsoft.com>
2021-03-01 18:46:36 +0530

    firebase setup

---
+++ src/fire.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyD6LvuaRIPK1KPxU1cYqTEn901lQpwbKrg",
+  authDomain: "get-set-data-from-firebase.firebaseapp.com",
+  projectId: "get-set-data-from-firebase",
+  storageBucket: "get-set-data-from-firebase.appspot.com",
+  messagingSenderId: "271333070970",
+  appId: "1:271333070970:web:08bdd15a21a495582783c3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
