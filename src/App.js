ae801dd51c6dc478ccc977124620d85b63163c67
Ravender Jadon <ravender@jnexsoft.com>
2021-03-02 12:20:07 +0530

    raed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { db } from "./fire";
+import firebase from "firebase";
+
+const useLogger = (logItem) => {
+  useEffect(() => {
+    console.log("%c Change accured ----------------------", "color: green;");
+    console.log(logItem);
+    console.log("%c Change finished ----------------------", "color: red;");
+  }, [logItem]);
+};
 
 function App() {
   const [inputState, setInputState] = useState("");
+  const [listInputState, setListInputState] = useState([]);
+
+  useLogger(listInputState);
+
+  // console.log("listInputState", listInputState);
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const addToFireDB = (e) => {
     e.preventDefault();
+    if (inputState !== "") {
+      db.collection("todos").add({
+        inprogress: true,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        todo: inputState,
+      });
+    }
+    setInputState("");
+  };
+
+  const getData = () => {
+    db.collection("todos").onSnapshot((querySnapshot) => {
+      setListInputState(
+        querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          todo: doc.data().todo,
+          inprogress: doc.data().inprogress,
+        }))
+      );
+    });
   };
 
   return (
@@ -20,6 +59,15 @@ function App() {
         />
         <button type="submit">Add</button>
       </form>
+      <div>
+        {listInputState.map((item) => {
+          return (
+            <div>
+              <p>{item.todo}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
